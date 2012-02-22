5c4555d75c8185c313e89dcc23c40390ea0e2dfb
minghz <m.huazhao@gmail.com>
2012-02-21 21:38:53 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ed3::Application.config.secret_token = 'd86e6c5b9cb4acd28b485b0aab3e03b2334d2ae89180191acae7bb5d2f6660d2abaf6d022cfc5eea21aafa0f6c910aecc32d662d350bd6892874ecabd7d93975'
