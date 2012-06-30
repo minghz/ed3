e4d3f01e5038912fa50bef78a6d9d17c564ef15b
Minghua Zhao <m.huazhao@gmail.com>
2012-06-30 18:05:45 -0400

    adding tag features

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
+  attr_accessible :content, :name, :title, :tags_attributes
+
   validates :name,  :presence => true
   validates :title, :presence => true,
                     :length => { :minimum => 5 }
@@ -7,4 +9,8 @@ class Post < ActiveRecord::Base
                        :length => { :maximum => 20 }
 
   has_many :comments, :dependent => :destroy
+  has_many :tags
+
+  accepts_nested_attributes_for :tags, :allow_destroy => :true, :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
+
 end
