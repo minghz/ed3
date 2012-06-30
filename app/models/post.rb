45c7dbabaf5788a75c114830a0973d39628c45ec
Minghua Zhao <m.huazhao@gmail.com>
2012-06-30 18:44:05 -0400

    adding support to delete tags with posts as well

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,7 @@ class Post < ActiveRecord::Base
                        :length => { :maximum => 20 }
 
   has_many :comments, :dependent => :destroy
-  has_many :tags
+  has_many :tags, :dependent => :destroy
 
   accepts_nested_attributes_for :tags, :allow_destroy => :true, :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
 
