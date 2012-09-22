61c695842025de71e291ace9f1fa21adc90faeca
Minghua Zhao <m.huazhao@gmail.com>
2012-09-22 11:14:29 -0400

    + user redirect if not signed in, and users list

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
+    user = User.find_by_email(params[:email].downcase)
     if user && user.authenticate(params[:password])
       #succecefful sign in
       sign_in user
-      redirect_to user
+      redirect_back_or user
   
     else
       #something went wrong, re-render the sign up form
